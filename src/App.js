d88c5b29880509bcf6486f25cb7358af931ffa45
samijelliti <samijelliti@yahoo.fr>
2019-01-22 16:23:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Timer from "./timer.js";
-
+const count ="29080089"
 class App extends Component {
   render() {
     return (
       <div className="App">
         
-        <Timer/>
+        <Timer  time={count}/>
       </div>
       
     );
--- src/timer.js
+++ src/timer.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import './timer.css'
 
+
+
+
 class Timer extends Component {
-    render(){
+  
+    render(){ 
+       const {time}=this.props
+       let heure=Math.trunc(time/3600000)
+       let resteheure=time-(heure*3600000)
+       let minute=Math.trunc(resteheure/60000)
+       let resteminute=resteheure-(minute*60000)
+       let seconde=Math.trunc(resteminute/1000)
         return(<div className="horlog">
         <div className="numheure1">
-            <p className="numh">00</p>
+            <p className="numh">{heure}</p>
             <p className="lettre">Hour</p>
          </div>
          <div className="numheure2">
-            <p className="numh">:00:</p>
+            <p className="numh">:{minute}:</p>
             <p className="lettre">Minute</p>
          </div>
          <div className="numheure3">
-            <p className="numh">00</p>
+            <p className="numh">{seconde}</p>
             <p className="lettre">Second</p>
          </div>
          </div>
